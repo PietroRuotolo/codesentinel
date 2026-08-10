c757dc015e0e2eafdce39c1ff5b6e8e482649593
Pietro Ruotolo <pietroschimidt03082007@gmail.com>
2026-08-10 16:41:16 -0300

    refactor: modela log level como enum e usa record para LogEntry

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,14 @@
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Optional;
 
 public class Main {
+
+    private final static List<LogEntry> logEntryList = new ArrayList<>();
+
     public static void main(String[] args){
         try(BufferedReader br = new BufferedReader(new FileReader("./logs.txt"))) {
             filterByError(br);
@@ -14,28 +19,41 @@ public class Main {
 
     public static void filterByError(BufferedReader br) throws IOException {
         String line;
-
         LogEntry entry;
-        int errorCounter = 0;
-        int lineCounter = 0;
+        int badFormatCount = 0;
         while ((line = br.readLine()) != null){
             Optional<LogEntry> optionalEntry = LogEntry.from(line);
             if(optionalEntry.isPresent()){
                 entry = optionalEntry.get();
-                if(entry.isError()){
-                    System.out.println(entry);
-                    errorCounter++;
-                }
+                saveLog(entry);
             }else{
-                System.err.println("* Bad Log Format *");
+                System.err.println("Bad log format");
+                badFormatCount++;
             }
-            lineCounter++;
         }
-        printErrorLog(errorCounter, lineCounter);
+        List<LogEntry> errorList = logEntryList.stream().filter(LogEntry::isError)
+                        .toList();
+
+        printLog(errorList, LogTypes.ERRO);
+        printLogInfo(errorList.size(), logEntryList.size(), badFormatCount);
     }
 
-    public static void printErrorLog(int errorCounter, int lineCounter){
-        System.out.println("Foram indentificados um total de " + errorCounter + " erros em " + lineCounter + " linhas.");
+    public static void printLog(List<LogEntry> logEntryList, LogTypes header){
+
+        System.out.println("-".repeat(10));
+        System.out.println("LISTA DE LOGS");
+        System.out.println("TIPO: " + "[" + header + "]");
+        System.out.println("-".repeat(10));
+        logEntryList.forEach(System.out::println);
+    }
+
+    public static void saveLog(LogEntry entry){
+        logEntryList.add(entry);
+    }
+
+    public static void printLogInfo(long errorCounter, int lineCounter, int badFormatCount){
+        System.out.printf("Foram indentificados um total de %d erros em %d linhas.%nUm total de %d linhas não foram possíveis de serem lidas.%n",
+                errorCounter, lineCounter, badFormatCount);
     }
 }
 
