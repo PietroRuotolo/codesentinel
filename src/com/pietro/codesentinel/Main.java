df796dc5b67af72817a2fe0d9236e68deda1d399
Pietro Ruotolo <pietroschimidt03082007@gmail.com>
2026-08-12 16:26:25 -0300

    refactor: Refatora código da main para LogAnalyzer

--- src/com/pietro/codesentinel/Main.java
+++ src/com/pietro/codesentinel/Main.java
@@ -1,77 +1,24 @@
 package com.pietro.codesentinel;
 
-import com.pietro.codesentinel.model.LogEntry;
-import com.pietro.codesentinel.model.LogTypes;
+import com.pietro.codesentinel.service.LogAnalyzer;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.Scanner;
 
 public class Main {
-
-    private final static List<LogEntry> logEntryList = new ArrayList<>();
-    private static Map<String, Long> exceptionMap;
-
+    //./logs.txt
+    private static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args){
-        try(BufferedReader br = new BufferedReader(new FileReader("./logs.txt"))) {
-            filterByError(br);
+        System.out.print("Copy the file path here: ");
+        String fileName = scanner.nextLine();
+        try(BufferedReader br = new BufferedReader(new FileReader(fileName))) {
+            LogAnalyzer errorAnalyzer = new LogAnalyzer();
+            errorAnalyzer.filterByError(br);
         }catch (IOException e){
             System.out.println("An error occurred: " + e.getMessage());
         }
-    }
-
-    public static void filterByError(BufferedReader br) throws IOException {
-        String line;
-        LogEntry entry;
-        int badFormatCount = 0;
-        while ((line = br.readLine()) != null){
-            Optional<LogEntry> optionalEntry = LogEntry.from(line);
-            if(optionalEntry.isPresent()){
-                entry = optionalEntry.get();
-                saveLog(entry);
-            }else{
-                System.err.println("Bad log format");
-                badFormatCount++;
-            }
-        }
-        List<LogEntry> errorList = logEntryList.stream().filter(LogEntry::isError)
-                        .toList();
-
-        groupException();
-        printLog(errorList, LogTypes.ERRO);
-        printLogInfo(errorList.size(), logEntryList.size(), badFormatCount);
-    }
-
-    private static void groupException(){
-        exceptionMap = logEntryList.stream()
-                .collect(Collectors.groupingBy(entry -> entry.getExceptionType().orElse("Not a valid exception"), Collectors.counting()));
-    }
-
-    public static void printLog(List<LogEntry> logEntryList, LogTypes header){
-
-        System.out.println("-".repeat(30));
-        System.out.println("LOG LIST");
-        System.out.println("TYPE: " + "[" + header + "]");
-        System.out.println("-".repeat(30));
-        logEntryList.forEach(System.out::println);
-        System.out.println("-".repeat(30));
-
-        System.out.println("More Details: ");
-        System.out.println("-".repeat(30));
-        exceptionMap.forEach((k,v) -> System.out.println(k + " : " + v));
-        System.out.println("-".repeat(30));
-
-    }
-
-    public static void saveLog(LogEntry entry){
-        logEntryList.add(entry);
-    }
-
-    public static void printLogInfo(long errorCounter, int lineCounter, int badFormatCount){
-        System.out.printf("It was found a total of %d errors in %d lines.%nA total of %d lines could not been read.%n",
-                errorCounter, lineCounter, badFormatCount);
     }
 }
 
--- src/com/pietro/codesentinel/service/LogAnalyzer.java
+++ src/com/pietro/codesentinel/service/LogAnalyzer.java
@@ -1,4 +1,68 @@
 package com.pietro.codesentinel.service;
 
+import com.pietro.codesentinel.model.LogEntry;
+import com.pietro.codesentinel.model.LogTypes;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class LogAnalyzer {
+
+    private final List<LogEntry> logEntryList = new ArrayList<>();
+
+    public void saveLog(LogEntry entry){
+        logEntryList.add(entry);
+    }
+
+    private Map<String, Long> groupExceptions(List<LogEntry> errorList){
+         return errorList.stream()
+                 .map(LogEntry::getExceptionType)
+                 .flatMap(Optional::stream)
+                 .collect(Collectors.groupingBy(type -> type, Collectors.counting()));
+    }
+
+    public void filterByError(BufferedReader br) throws IOException {
+        String line;
+        LogEntry entry;
+        int badFormatCount = 0;
+        while ((line = br.readLine()) != null){
+            Optional<LogEntry> optionalEntry = LogEntry.from(line);
+            if(optionalEntry.isPresent()){
+                entry = optionalEntry.get();
+                saveLog(entry);
+            }else{
+                System.err.println("Bad log format");
+                badFormatCount++;
+            }
+        }
+
+        List<LogEntry> errorList = logEntryList.stream().filter(LogEntry::isError)
+                .toList();
+        Map<String, Long> exceptions = groupExceptions(errorList);
+        printLog(errorList, exceptions ,LogTypes.ERRO);
+        printLogInfo(errorList.size(), logEntryList.size(), badFormatCount);
+    }
+
+    public void printLog(List<LogEntry> logEntryList, Map<String, Long> logMap ,LogTypes header){
+
+        System.out.println("-".repeat(30));
+        System.out.println("LOG LIST");
+        System.out.println("TYPE: " + "[" + header + "]");
+        System.out.println("-".repeat(30));
+        logEntryList.forEach(System.out::println);
+        System.out.println("-".repeat(30));
+
+        System.out.println("More Details: ");
+        System.out.println("-".repeat(30));
+        logMap.forEach((k,v) -> System.out.println(k + " : " + v));
+        System.out.println("-".repeat(30));
+    }
+
+    public void printLogInfo(long errorCounter, int lineCounter, int badFormatCount){
+
+        System.out.printf("It was found a total of %d errors in %d lines.%nA total of %d lines could not been read.%n",
+                errorCounter, lineCounter, badFormatCount);
+    }
 }
