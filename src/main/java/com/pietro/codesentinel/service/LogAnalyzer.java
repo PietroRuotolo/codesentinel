e5257e15fc4d571435275d0d09e3f30db32aba7e
Pietro Ruotolo <pietroschimidt03082007@gmail.com>
2026-08-20 17:31:21 -0300

    feat: Adiciona endpoint POST para analisar conteúdo de log pelo request body

--- src/main/java/com/pietro/codesentinel/service/LogAnalyzer.java
+++ src/main/java/com/pietro/codesentinel/service/LogAnalyzer.java
@@ -1,43 +1,20 @@
 package com.pietro.codesentinel.service;
 
 import com.pietro.codesentinel.model.LogEntry;
-import com.pietro.codesentinel.model.LogTypes;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-import org.springframework.stereotype.Component;
-import org.springframework.stereotype.Repository;
 import org.springframework.stereotype.Service;
 
-import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 import java.util.stream.Collectors;
 
 @Service
 public class LogAnalyzer {
 
-    private final List<LogEntry> logEntryList = new ArrayList<>();
-
-    public Map<String, Long> analyzeFile(String path){
-        Resource resource = new ClassPathResource(path);
-        try (BufferedReader br = new BufferedReader(new InputStreamReader(resource.getInputStream(), StandardCharsets.UTF_8))) {
-            String line;
-            while((line = br.readLine()) != null){
-                Optional<LogEntry> optionalLogEntry = LogEntry.from(line);
-                optionalLogEntry.ifPresent(this::saveLog);
-            }
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
-
-        List<LogEntry> errorList = logEntryList.stream()
+    public Map<String, Long> analyzeErrors(String text){
+        List<LogEntry> errorList = text.lines()
+                .map(LogEntry::from).flatMap(Optional::stream)
                 .filter(LogEntry::isError)
                 .toList();
         return groupExceptions(errorList);
-    }
-
-    public void saveLog(LogEntry entry){
-        logEntryList.add(entry);
     }
 
     private Map<String, Long> groupExceptions(List<LogEntry> errorList){
@@ -45,20 +22,5 @@ public class LogAnalyzer {
                  .map(LogEntry::getExceptionType)
                  .flatMap(Optional::stream)
                  .collect(Collectors.groupingBy(type -> type, Collectors.counting()));
-    }
-
-    public void exportToCsv(Map<String, Long> logMap){
-        try(FileWriter fw = new FileWriter("analise/logdata.csv");
-            BufferedWriter bw = new BufferedWriter(fw)){
-                bw.write("exception_type,count");
-                bw.newLine();
-                for(var entry : logMap.entrySet()){
-                    bw.write(entry.getKey() + "," + entry.getValue());
-                    bw.newLine();
-                }
-            System.out.println("Map saved with success!");
-        }catch (IOException e){
-            System.err.println("Geral error in the file: " + e.getMessage());
-        }
     }
 }
